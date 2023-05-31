8bf4d0e38bae2a5db5b4dd425226e9746c7ed83c
Thonk <75491816+TechnoShip123@users.noreply.github.com>
2023-05-31 11:13:37 -0500

    Cleanup, create window and renderer simultaneously

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,46 @@
-#include <stdio.h>
+#include <iostream>
 #include <SDL2/SDL.h>
+
+using std::printf;
+using std::string;
 
 // Screen dimension constants
 const int SCREEN_WIDTH = 856;
 const int SCREEN_HEIGHT = 482;
 
+// Window and Renderer
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
 int main(int argc, char* args[]) {
-    SDL_Window* window = NULL;
-
-    // The surface that the window holds
-    SDL_Surface* screenSurface = NULL;
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    #pragma region Initialize SDL, window, renderer, etc.
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         printf("Error initializing SDL: %s\n", SDL_GetError()); return EXIT_FAILURE;
     }
-    printf("SDL init successful!\n");
+    printf("SDL init successful.\n");
 
-    //Create window
-    window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-    if (window == NULL) {
-        printf("Failed to create window: %s\n", SDL_GetError()); return EXIT_FAILURE;
+    if (SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer) != 0) {
+        printf("Error initializing window and renderer: %s\n", SDL_GetError()); return EXIT_FAILURE;
     }
-
-    // Get surface, fill with red, and then push the changes by updating the surface.
-    screenSurface = SDL_GetWindowSurface( window );
-    SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0, 0 ) );
-
-    SDL_UpdateWindowSurface( window );
+    SDL_SetWindowTitle(window, "LowLevelGame");
+    printf("Window and renderer init successful.\n");
+    #pragma endregion
 
     // Keep the window open until the user closes it (we will recieve a close event)
-    SDL_Event e; bool quit = false; 
-    while (quit == false) {
+    SDL_Event e; bool shouldQuit = false;
+    while (shouldQuit == false) {
+        #pragma region Event Handling
         while (SDL_PollEvent(&e)) { 
-            if(e.type == SDL_QUIT) quit = true;
+            if (e.type == SDL_QUIT) shouldQuit = true;
         }
+        #pragma endregion
+
+        // TODO: Move game updates to their own Update() function?
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0, 0xFF);
+        SDL_RenderClear(renderer);
+
+        // Draw
+        SDL_RenderPresent(renderer);
     }
 
     // Exit tasks
